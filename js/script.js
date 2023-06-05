d32b0a7d3965a2357c715a6a366fdb269d6ab54b
Ayuka Saijo <ayuka.saijo.0629@gmail.com>
2023-06-06 00:17:41 +0900

    0605 add message

--- js/script.js
+++ js/script.js
@@ -8,4 +8,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
       currentSlide = (currentSlide + 1) % slides.length;
       slides[currentSlide].classList.add("active");
     }
+
   });
+ // ページが読み込まれたときに実行される関数
+ function fadeElements() {
+  var fadeElements = document.querySelectorAll('.fade-in');
+  
+  // スクロールイベントのハンドラ関数
+  function fade() {
+    fadeElements.forEach(function(element) {
+      var elementTop = element.getBoundingClientRect().top;
+      var elementBottom = element.getBoundingClientRect().bottom;
+      
+      // ウィンドウの中央に要素が表示された場合にフェードイン
+      if (elementTop < window.innerHeight && elementBottom >= 0) {
+        element.style.opacity = "1";
+      }
+    });
+  }
+    fade();
+    
+    // スクロールイベントのリスナーを追加
+    window.addEventListener('scroll', fade);
+  }
+  
+  // ページが読み込まれたときにfadeElements関数を実行
+  window.addEventListener('load', fadeElements);
+
+
+
+  
